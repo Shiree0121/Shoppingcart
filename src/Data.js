fbbba6bc3422555ee84db2574e8cba3d027930b4
shiree0121 <shirewhite@hotmail.com>
2024-06-13 15:57:41 +0100

    first commmit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+/**
+ * ! data to populate the shopping cart
+ */
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Mangos",
+      price: 415.63,
+      img: "images/mangos.png",
+    },
+    {
+      id: "trfoiwfcnbcawdjksadjk",
+      name: "Dragon Fruit",
+      price: 600.42,
+      img: "images/dragonfruit.png",
+    },
+    {
+      id: "cbvxbcvsceldk",
+      name: "Oranges",
+      price: 25.99,
+      img: "images/oranges.png",
+    },
+    {
+      id: "oiopijmjkhuihb",
+      name: "Passion Fruit",
+      price: 120.23,
+      img: "images/passionfruit.png",
+    },
+  ];
+  
